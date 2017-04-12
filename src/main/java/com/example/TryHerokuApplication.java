05dbd643e9322ec8cc90fd9b8e23fe8eaf59f713
大畔祐輝 <ooguro@ooguro.local>
2017-04-12 19:51:45 +0900

    initializr

---
+++ src/main/java/com/example/TryHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryHerokuApplication.class, args);
+	}
+}
